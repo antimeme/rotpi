415c918da40727e70966fe73ec0bd41d86602e6c
Jeff Gold <jgold@esclat.net>
2015-07-05 22:21:14 -0400

    Created simplistic Node.js web server

---
+++ server.js
@@ -0,0 +1,46 @@
+#! /usr/bin/env node
+// Quick-and-dirty web server using Node.js
+var sys = require('sys');
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var port = 8080;
+
+http.createServer(function(request, response) {
+    var url = request.url;
+    if (url === '/')
+        url = '/index.html';
+
+    if (url === '/time') {
+        response.writeHeader(200, {
+            "Context-Type": "text/plain",
+            "Access-Control-Allow-Origin": "http://localhost/"});
+        response.write(new Date().toString());
+        response.end();
+    }
+    
+    fs.readFile(path.join('.', url), function (err, data) {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                fs.readFile('page404.html', function(err, data) {
+                    if (err)
+                        throw err;
+                    response.writeHeader(404, {
+                        "Content-Type": "text/html"});
+                    response.write(data.toString()
+                                   .replace(/:PATH:/g, url));
+                    response.end();
+                });
+            } else throw err;
+        } else {
+            // :TODO: determine content type --
+            //   by file extension?
+            //   by contents?
+            //   a combination of both?
+            response.writeHeader(200, {"Content-Type": "text/html"});
+            response.write(data);
+            response.end();
+        }
+    });
+}).listen(port);
+console.log('Server listening: http://localhost:' + port + '/');
